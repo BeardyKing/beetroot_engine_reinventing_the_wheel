a938907ee40d39f33afc646e36306d8982ae97f6
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2023-06-24 19:19:13 +0100

    [PIPELINE] Added third-party fonts to project
    Added Fonts
    - JetBrainsMono
    - Unifont 15.0.06
    Both under the Open Font Licence 1.1
    
    Renamed info.txt -> font_distribution_info.txt
    - Included general information about the font resource
    
    Pipeline
    - Updated path to font resources

--- preprocess_pipeline/main.cpp
+++ preprocess_pipeline/main.cpp
@@ -2,8 +2,8 @@
 #include <pipeline/font_atlas.h>
 
 void build_font_atlas_and_description() {
-    pipeline_build_font_atlas(PIPELINE_FONT_DIR, "JetBrainsMono-Regular", ".ttf", 48, 512);
-    pipeline_build_font_atlas(PIPELINE_FONT_DIR, "unifont-15.0.06", ".ttf", 48, 512);
+    pipeline_build_font_atlas(PIPELINE_FONT_DIR, "JetBrainsMono/JetBrainsMono-Regular", ".ttf", 48, 512);
+    pipeline_build_font_atlas(PIPELINE_FONT_DIR, "Unifont/unifont-15.0.06", ".ttf", 48, 512);
 }
 
 int main() {
